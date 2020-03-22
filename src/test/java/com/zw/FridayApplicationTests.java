271a46f77514ca926525adf575e48b32b8f078b7
zwfirefly <zwfirefly@163.com>
2020-03-22 17:52:17 +0800

    我的第一次提交

---
+++ src/test/java/com/zw/FridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
