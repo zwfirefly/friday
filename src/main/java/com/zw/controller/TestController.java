5ee65c2f643ce24a4859f845d09df678000a7bb7
zwfirefly <zwfirefly@163.com>
2020-03-22 19:52:44 +0800

    我的第一次提交

---
+++ src/main/java/com/zw/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.zw.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "测试controller是否正常";
+    }
+}
